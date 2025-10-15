246a15c0d5be673427fe689741a10a8c870c0c6c
Jorge Gonzalez <jorge.gonzalez.renedo@gmail.com>
2025-10-15 10:35:50 +0200

    Hello world qiskit para ver circuito

--- qiskit/circuito_prueba.py
+++ qiskit/circuito_prueba.py
@@ -2,6 +2,7 @@ from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import EstimatorV2 as Estimator
+import matplotlib.pyplot as plt
  
 # Create a new circuit with two qubits
 qc = QuantumCircuit(2)
@@ -18,3 +19,4 @@ qc.cx(0, 1)
 # If you're running this in a script, use `print(qc.draw())` to
 # print a text drawing.
 qc.draw("mpl")
+plt.show()
