5ea4ad3f6730a62875758f45b0dd1be1b97fd3c5
Jorgonre <106432897+Jorgonre@users.noreply.github.com>
2025-11-27 15:19:08 +0000

    Toffoli para qiskit y mal en cirq

--- benchmark.py
+++ benchmark.py
@@ -166,7 +166,7 @@ def main():
     ensure_results_dir()
 
     resultados = {}
-    circuitos = ["circuito_prueba.py", "adder_n4.py"]
+    circuitos = ["circuito_prueba.py", "adder_n4.py","toffoli_n3.py"]
 
     for circuito in circuitos:
         duracion = run_framework(circuito)
---
+++ qiskit/toffoli_n3.py
@@ -0,0 +1,105 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+from numpy import pi
+import time
+from qiskit import transpile
+import psutil
+import os
+
+process = psutil.Process(os.getpid())
+
+process.cpu_percent(interval=None)  # Establece línea base
+
+start_time = time.time()
+# Medir tiempo de construcción
+start_build = time.time()
+
+qreg_a = QuantumRegister(3, 'a')
+creg_c = ClassicalRegister(3, 'c')
+circuit = QuantumCircuit(qreg_a, creg_c)
+
+circuit.x(qreg_a[0])
+circuit.x(qreg_a[1])
+circuit.h(qreg_a[2])
+
+circuit.cx(qreg_a[1], qreg_a[2])
+
+circuit.tdg(qreg_a[2])
+
+circuit.cx(qreg_a[0], qreg_a[2])
+
+circuit.t(qreg_a[2])
+
+circuit.cx(qreg_a[1], qreg_a[2])
+
+circuit.tdg(qreg_a[2])
+circuit.tdg(qreg_a[1])
+
+circuit.cx(qreg_a[0], qreg_a[2])
+
+circuit.t(qreg_a[2])
+circuit.cx(qreg_a[0], qreg_a[1])
+
+circuit.h(qreg_a[2])
+circuit.tdg(qreg_a[1])
+
+circuit.cx(qreg_a[0], qreg_a[1])
+
+circuit.t(qreg_a[0])
+circuit.s(qreg_a[1])
+
+circuit.measure(qreg_a[0], creg_c[0])
+circuit.measure(qreg_a[1], creg_c[1])
+circuit.measure(qreg_a[2], creg_c[2])
+
+build_time = time.time() - start_build
+
+start_transpile = time.time()
+
+sim = AerSimulator()
+transpiled = transpile(circuit, backend=sim)
+
+job = sim.run(transpiled, shots=1024)  
+result = job.result()
+
+transpile_time = time.time() - start_transpile
+
+# Medir uso de recursos
+cpu_usage = process.cpu_percent(None)
+ram_usage_mb = process.memory_info().rss / (1024 * 1024)
+
+counts = result.get_counts()
+
+total_time = time.time() - start_time
+
+print(counts)
+ 
+plot_histogram(counts)
+
+# Contar operaciones
+ops = circuit.count_ops()
+
+# Clasificar puertas
+one_qubit_gates = ['x', 'h', 't', 'tdg', 's']
+two_qubit_gates = ['cx']
+
+num_1q = sum(ops.get(gate, 0) for gate in one_qubit_gates)
+num_2q = sum(ops.get(gate, 0) for gate in two_qubit_gates)
+total_gates = num_1q + num_2q
+num_qubits = circuit.num_qubits
+depth = circuit.depth()
+
+
+print("# --- METRICS ---")
+print(f"Qubits:{num_qubits}")
+print(f"Depth:{depth}")
+print(f"Gate_1q:{num_1q}")
+print(f"Gate_2q:{num_2q}")
+print(f"Total_gates:{total_gates}")
+print(f"Build_time:{build_time:.4f}")
+print(f"Transpile_execution_time:{transpile_time:.4f}")
+print(f"Total_time:{total_time:.4f}")
+print(f"CPU_usage:{cpu_usage:.2f}")
+print(f"RAM_usage_MB:{ram_usage_mb:.2f}")
+print("# --- END_METRICS ---")
