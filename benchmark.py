3bad4cbce7443d3cf644a84526429430a38b0bbc
Jorgonre <106432897+Jorgonre@users.noreply.github.com>
2026-01-12 10:19:04 +0000

    Diferenciación de pequeños y medianos en benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -167,7 +167,8 @@ def main():
     # Obtener nombre del framework basado en la carpeta actual
     framework_name = os.path.basename(os.getcwd())
 
-    #circuitos = ["adder_n4.py","toffoli_n3.py","qft_n4.py", "bell_n4.py", "seca_n11.py", "bigadder_n18.py", "dnn_n16.py"]
+    #circuitos = ["adder_n4.py","toffoli_n3.py","qft_n4.py", "bell_n4.py"] # Circuitos pequeños
+    #circuitos = ["seca_n11.py", "bigadder_n18.py", "dnn_n16.py"] #Circuitos medianos
     circuitos = ["dnn_n16.py"] # Descomentar para pruebas rápidas
 
     for circuito in circuitos:
