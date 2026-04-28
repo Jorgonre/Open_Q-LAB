4e782bb22f7263dd36b1c806e65254c4589ef98d
Jorgonre <106432897+Jorgonre@users.noreply.github.com>
2026-04-28 09:05:43 +0000

    Ajustes para redacción y memoria

--- qiskit/circuito_prueba.py
+++ qiskit/circuito_prueba.py
@@ -1,22 +1,25 @@
+#Librerías a importar
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import EstimatorV2 as Estimator
 import matplotlib.pyplot as plt
  
-# Create a new circuit with two qubits
+#Inicialización del circuito
 qc = QuantumCircuit(2)
  
-# Add a Hadamard gate to qubit 0
+#Desarrollo del circuito
 qc.h(0)
- 
-# Perform a controlled-X gate on qubit 1, controlled by qubit 0
 qc.cx(0, 1)
- 
-# Return a drawing of the circuit using MatPlotLib ("mpl").
-# These guides are written by using Jupyter notebooks, which
-# display the output of the last line of each cell.
-# If you're running this in a script, use `print(qc.draw())` to
-# print a text drawing.
+qc.measure_all()
+
+#Ejecución del circuito
+sim = AerSimulator()
+transpiled = transpile(qc, backend=sim, optimization_level = 0)
+job = sim.run(transpiled, shots=1024)  
+result = job.result()
+
+
+
 qc.draw("mpl")
 plt.show()
